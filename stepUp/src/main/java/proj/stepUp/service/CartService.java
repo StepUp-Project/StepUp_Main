5d11256dbba81514097009feb708a301417a57cb
jinhoon132 <117323445+jinhoon132@users.noreply.github.com>
2023-04-15 21:53:11 +0900

    CartService업뎃
    업뎃

--- stepUp/src/main/java/proj/stepUp/service/CartService.java
+++ stepUp/src/main/java/proj/stepUp/service/CartService.java
@@ -1,7 +1,11 @@
 package proj.stepUp.service;
 
+import java.util.List;
+
 import proj.stepUp.vo.CartVO;
 
 public interface CartService {
-	public int insertCart(CartVO vo);
+	public List<CartVO> list(int userIndex);
+	public int deleteCart(int cartIndex);
+
 }
