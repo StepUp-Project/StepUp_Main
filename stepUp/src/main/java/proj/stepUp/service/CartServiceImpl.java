e326cde1b06426c7d6f567fa954e6a8a26400385
jinhoon132 <117323445+jinhoon132@users.noreply.github.com>
2023-04-15 21:53:48 +0900

    삭제까진 추가

--- stepUp/src/main/java/proj/stepUp/service/CartServiceImpl.java
+++ stepUp/src/main/java/proj/stepUp/service/CartServiceImpl.java
@@ -1,5 +1,7 @@
 package proj.stepUp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,14 +9,20 @@ import proj.stepUp.dao.CartDAO;
 import proj.stepUp.vo.CartVO;
 
 @Service
-public class CartServiceImpl implements CartService {
-	
+public class CartServiceImpl implements CartService{
+
 	@Autowired
 	private CartDAO cartDAO;
 	
 	@Override
-	public int insertCart(CartVO vo) {
-		return cartDAO.insertCart(vo);
+	public List<CartVO> list(int userIndex){
+		return cartDAO.list(userIndex);
 	}
-	
+
+	@Override
+	public int deleteCart(int cartIndex) {
+		return cartDAO.deleteCart(cartIndex);
+	}
+
+
 }
